a98bb98f8b96422360ed3afb8f72335b787a45e4
zzzzquan <1215721590@qq.com>
2025-11-25 21:28:35 +0800

    删库重写

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Vercel deploy entry handler, for serverless deployment, please don't modify this file
- */
-import app from './app.ts';
-
-export default function handler(req: any, res: any) {
-  return app(req, res);
-}
